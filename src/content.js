c850489af7694ec1a0b47341cbf1db680504e912
Ohad Vano <ohadvano@gmail.com>
2024-06-03 23:58:28 +0300

    Tests

--- src/content.js
+++ src/content.js
@@ -1,44 +1,43 @@
 import { RuleType } from './consts.js';
 
 chrome.runtime.onMessage.addListener((renameRequest, _sender, sendResponse) => {
-    getRequestedElement(renameRequest)
-        .then((element) => {
-            console.log('Found requested element');
-            const newTitle = element.innerHTML;
-            document.title = newTitle;
+    getRequestedElement(renameRequest).then((element) => {
+        console.log('Found requested element');
+        const newTitle = element.innerHTML;
+        document.title = newTitle;
 
-            let titleObserver = new MutationObserver((mutations) => {
-                mutations.forEach((mutation) => {
-                    if (mutation.type != 'childList') {
-                        return;
-                    }
+        let titleObserver = new MutationObserver((mutations) => {
+            mutations.forEach((mutation) => {
+                if (mutation.type != 'childList') {
+                    return;
+                }
 
-                    if (document.title !== newTitle) {
-                        document.title = newTitle;
-                    }
-                });
+                if (document.title !== newTitle) {
+                    document.title = newTitle;
+                }
             });
-
-            titleObserver.observe(document.querySelector('title'), {
-                childList: true
-            });
-
-            sendResponse({ result: "ok" });
-        })
-        .catch((error) => {
-            console.warn(error);
-            sendResponse({ result: "fail" });
         });
+
+        titleObserver.observe(document.querySelector('title'), {
+            childList: true
+        });
+
+        sendResponse({ result: "ok" });
+    }).catch((error) => {
+        console.warn(error);
+        sendResponse({ result: "fail" });
+    });
 });
 
 function getRequestedElement(renameRequest) {
     return new Promise((resolve, reject) => {
         function getElement(renameRequest) {
             switch (renameRequest.type) {
-                case RuleType.Fixed:
+                case RuleType.Fixed: {
                     const newElement = document.createElement('section');
                     newElement.textContent = renameRequest.value;
                     return newElement;
+                }
                 case RuleType.ElementId:
                     return document.getElementById(renameRequest.value);
                 case RuleType.ClassId:
--- src/rules.js
+++ src/rules.js
@@ -1,3 +1,4 @@
+import 'urlpattern-polyfill';
 import { RuleType } from './consts.js';
 
 export class RenameDefinition {
